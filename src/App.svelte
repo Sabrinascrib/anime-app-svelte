08be4201770f60341510d50d70127ad96dfcae76
sabrina <sabrinaabrodi20@gmail.com>
2022-10-29 21:39:44 +0200

    feat: displaying anime data

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,20 @@
+
 <script>
+  import { onMount } from 'svelte'
+
   let animes = []
 
-  fetch('https://api.jikan.moe/v4/anime?q=naruto&sfw')
-  .then(response => response.json())
-  .then(data => console.log(data))
+  onMount(() => {
+    fetch('https://api.jikan.moe/v4/anime?q=naruto&sfw')
+      .then(response => response.json())
+       .then(result => animes = result.data)
+	//   .then(result => console.log(result.data))
+  })
 </script>
+
+{#each animes as anime (anime.mal_id)}
+
+  <img src="anime.images.jpg.large_image_url" alt="{anime.title}">
+
+  <h2>{anime.title}</h2>
+{/each}
