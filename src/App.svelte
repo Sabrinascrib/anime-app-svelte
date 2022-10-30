0a1e07eeeb986311a608effe1290f6878ed3558d
sabrina <sabrinaabrodi20@gmail.com>
2022-10-30 19:59:01 +0100

    feat: styling navbar

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,50 @@
 </main>
 <style>
 
+main {
+  font-family: 'Poppins', sans-serif;
+}
 
+  nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0px 50px;
+    align-items: center;
+    max-width: 1500px;
+    margin: 0 auto;
+  }
+
+  .logo {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .logo img {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+
+  .logo h2 span {
+    color: #33cccc;
+    font-style: italic;
+  }
+
+  input {
+    height: 30px;
+    width: 200px;
+    border: solid #e0e0e0 1px;
+    background-color: #f1f1f1;
+    padding: 2px 10px;
+    border-radius: 5px;
+  }
+
+  input:focus {
+border: solid #33cccc 1px;
+  }
 
 
 </style>
