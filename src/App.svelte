f6890f3e3b9819f7fa022239e0dfc1bb82fa5085
sabrina <sabrinaabrodi20@gmail.com>
2022-10-30 19:58:28 +0100

    feat: adding navbar/header

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,40 @@
 
 <script>
   import { onMount } from 'svelte'
-
+	let name = '';
   let animes = []
 
   onMount(() => {
-    fetch('https://api.jikan.moe/v4/anime?q=naruto&sfw')
+    fetch(`https://api.jikan.moe/v4/anime?q=${name}&sfw`)
       .then(response => response.json())
        .then(result => animes = result.data)
-	//   .then(result => console.log(result.data))
+	  // .then(result => console.log(result.data))
   })
 </script>
 
+<main>
+<nav>
+  <div class="logo">
+      <img src="./images/mie.png" alt="ramen logo" /> <h2>ramen <span> n' chill</span></h2>
+  </div>
+  <input bind:value={name} placeholder="Search for an anime...">
+</nav>
+
+<div class="animes">
 {#each animes as anime (anime.mal_id)}
-
-  <img src="anime.images.jpg.large_image_url" alt="{anime.title}">
-
-  <h2>{anime.title}</h2>
+<div class="anime">
+  <!-- <img src="anime.images.jpg.large_image_url" alt="{anime.title}"> -->
+   <img src="./images/naruto-shippuden-group-i129089.jpg" alt="{anime.title}">
+<p>{anime.title_japanese}</p>
+  <h2>{anime.title}</h2></div>
 {/each}
+
+</div>
+
+</main>
+<style>
+
+
+
+
+</style>
